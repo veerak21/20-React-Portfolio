74d34b76612e7425d1c4d51e912b222930a3a296
C M Santosh Kumar <pagadala.kalyani@gmail.com>
2023-04-04 12:23:01 +0100

    img path corrected

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import profileImage from "../assets/profile.jpg";
+import profileImg from "../assets/profile.jpg";
 function About() {
     return (
         <section id="about-me" className="my-5 intro">
@@ -11,7 +11,7 @@ function About() {
             <div className="flex-row">
                 <div class="intro-info">
                     <div class="intro-img">
-                        <img src={profileImage} style={{ width: "100%" }} alt="profile" />
+                        <img src={profileImg} style={{ width: "100%" }} alt="profile" />
                     </div>
                     <p>
                         I am a certified full stack web developer from the University of Birmingham, Birmingham, UK. I build and design visually appealing and comprehensive full portfolio websites. I use the grid and flexbox basics, and advanced interactions in my designs. I employ Visual Studio and GitHub to carefully inspect the steps of building, deployment and launching the sites. I have experience in designing webpages with responsive layouts that adapt to different devises and browser settings. I employ HTML, CSS and JavaScript technologies in my designs. I have been trained in Oracle and gained extensive experience in database management. Therefore, I am able to develop websites reflecting database features. Please visit my previous work to gauge the extent of my abilities.</p>
